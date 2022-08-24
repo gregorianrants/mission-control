fb3b6491fdc820a6ea329f13100bbdbf0ed96c78
gregor murray <gregorian_rants@hotmail.com>
2022-08-24 20:35:41 +0100

    added a server which will sit between pi and frontend, created a new front end for this server still kept the direct front end to the pi in a seperate folder.

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import path from "path";
+
+import express from 'express'
+const app = express();
+import http from 'http'
+const server = http.createServer(app);
+import {Server} from 'socket.io'
+const io = new Server(server);
+
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const staticPath = path.join(__dirname, "../frontend-for-backend/build")
+console.log(staticPath)
+
+
+app.use(express.static(staticPath));
+
+app.get("/*", function (req, res) {
+    const thePath = path.join(__dirname, "../frontend-for-backend/build", "index.html")
+    console.log(thePath)
+    res.sendFile(thePath);
+});
+
+io.on('connection', (socket) => {
+    socket.on('ping',()=>{
+        console.log('we got a ping')
+        socket.emit('pong')
+    })
+});
+
+io.on('ping',(socket)=>{
+
+})
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
